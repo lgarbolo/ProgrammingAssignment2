1240fc5aa0f3ac6d95d07591f6188d849478b26a
lgarbolo <82562646+lgarbolo@users.noreply.github.com>
2021-04-19 23:37:07 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,31 @@
-# Assignment for R programming Week 3
+# Assignment for R programming Week 3 Lexical Scoping; for peer review assignment
 
-# This function caches the inverse of the special matrix created 
-
-makeCacheMatrix <- function(x = matrix()) 
+makeCacheMatrix <- function(x = matrix()) #this function makes the special matrix in which it caches the data's inverse as shown in getinverse and setinverse
 {
-  k <- NULL
-  set <- function(y){
-    x <<- y
-    k <<- NULL
+  minv <- NULL ##this is where the cached information will be stored; as assigned, it is named variable minv
+  set <- function(y){ ##this serves to make the function in order to set the values in this given matrix
+    x <<- y  ## the <<- indicates that it is assigning a value in a special environment, which is the parent one
+    minv <<- NULL
 }
-# setinverse and getinverse are used in order to calculate the inverse of the matrix
-# meanwhile set and get are used to simply get the values of the assigned matrix
+  
+# set and get will retrieve the values of the assigned matrix; getinverse and setinverse will calculate the inverse of these retrieved values
 get <- function()x
-setInverse <- function(inverse) k <<- inverse
-getInverse <- function() k 
+setInverse <- function(inverse)minv <<- inverse
+getInverse <- function()minv 
 list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-# the function below then returns the calculated inverse of the matrix
+# the function below then returns the inverse of the matrix above as calculated
 
 cacheSolve <- function(x, ...) 
 {
-  k <- x$getInverse()
-  if(!is.null(k)){
+  minv <- x$getInverse()
+  if(!is.null(minv)){
     message("getting cached data")
-    return(k)
+    return(minv)
   }
   mat <- x$get()
-  k <- solve(mat,...)
-  x$setInverse(k)
-  k
+  minv <- solve(mat,...)
+  x$setInverse(minv)
+  minv
 }
